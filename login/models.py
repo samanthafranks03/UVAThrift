d6ee25f8c1638198fdee81beefe1a94b69a50d76
cur4af <cur4af@virginia.edu>
2025-10-16 21:33:29 -0400

    Updated models to include skeleton User and Admin classes as well as updating the Django admin site to reflect them

--- login/models.py
+++ login/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # Personal Data
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    # Extra Data
+    bio = models.TextField(blank=True)
+    interests = models.TextField(blank=True)
+    picture_url = models.URLField(blank=True)
+
+    def make_post(self):
+        print("Making a post placeholder!\n")
+        return
+
+class Admin(User):
+
+    def ban_user(self, target_user):
+        print("Banning user: " + target_user + "!\n")
+        return
+
+    pass
