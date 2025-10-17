1c2f2f85af8fb106e4e04035aa097c4e6f39929c
cur4af <cur4af@virginia.edu>
2025-10-17 14:33:24 -0400

    Refactored models for proper model inheritance

--- login/models.py
+++ login/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class BaseUser(models.Model):
+
+class User(models.Model):
     # Personal Info
     name = models.CharField(max_length=100, default="John Doe")
     email = models.EmailField(unique=True, primary_key=True)
@@ -9,13 +10,9 @@ class BaseUser(models.Model):
     interests = models.TextField(blank=True)
     picture_url = models.URLField(blank=True)
 
-    class Meta:
-        abstract = True
-
-class User(BaseUser):
     def make_post(self):
         print("Making a post placeholder!\n")
 
-class Admin(BaseUser):
+class Admin(User):
     def ban_user(self, target_user):
         print("Banning user: " + target_user + "!\n")
