c3c49e9b703b12364b6021f496aae36dff89b3a5
heebs1215 <147454213+heebs1215@users.noreply.github.com>
2025-10-17 23:48:25 -0400

    Added fucntionality to the ban button so when a user is banned they cant go into the main page, but get redirected to their profile where they see 'you are banned' under their name

--- market/views.py
+++ market/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from users.models import User
 
 # Create your views here.
 def home(request):
+    #if a logged-in user is banned, redirect them to their profile page
+    user_data = request.session.get('user_data')
+    if user_data:
+        email = user_data.get('email')
+        try:
+            u = User.objects.get(email=email)
+            if u.is_flagged:
+                # Ensure we have their profile URL in session
+                user_url = request.session.get('user_URL') or u.get_absolute_url()
+                request.session['user_URL'] = user_url
+                return redirect(user_url)
+        except User.DoesNotExist:
+            pass
+
     return render(request, 'home_page.html')
