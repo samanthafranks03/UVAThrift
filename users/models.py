ef2727e05b3ee5ceda69a29ad2e0d952378b2074
cur4af <cur4af@virginia.edu>
2025-11-07 16:31:04 -0500

    Added nickname field into User model to handle Sprint 5 feature change; added shallow implementation of profile boxes to test appearance

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 class User(models.Model):
     # Personal Info
     name = models.CharField(max_length=100, default="John Doe")
+    nickname = models.CharField(max_length=100, blank=True)
     email = models.EmailField(unique=True, primary_key=True)
     hashed_email = models.CharField(max_length=10, unique=True)  # Should be a unique hash per user
     # Extra Info
